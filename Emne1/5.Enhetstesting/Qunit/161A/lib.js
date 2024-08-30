494fb38f432949a187ae74cd527a5e814aa5582e
Tommy Mai <tommymaimai@gmail.com>
2024-08-30 20:32:28 +0200

    tests

--- Emne1/5.Enhetstesting/Qunit/161A/lib.js
+++ Emne1/5.Enhetstesting/Qunit/161A/lib.js
@@ -2,29 +2,20 @@ function fixText(text){
     if(!text) return text;
     let regexTemplateCharacter = /[A-Z]/gi;
     let regexTemplateWhiteSpace = /\s/gi;
-    if(regexTemplateWhiteSpace.test(text)){
+    let regexTemplateAtSign = /@/gi;
+
+    if(regexTemplateAtSign.test(text)){
+        newText = text.replace("@", ".@.");
+        return newText;
+    }
+    else if(regexTemplateWhiteSpace.test(text)){
         newText = text.trim();
         newText = newText.charAt(0).toUpperCase() + newText.slice(1);
         return newText;
     }
-    if(regexTemplateCharacter.test(text)){
+    else if(regexTemplateCharacter.test(text)){
         newText = text.charAt(0).toUpperCase() + text.slice(1)
         return newText;
     }
-
     return text;
 }
-
-
-// if(!text) return text;
-// let regexTemplateCharacter = /[A-Z]/gi;
-// let regexTemplateWhiteSpace = /\s/gi;
-
-// else if(regexTemplateWhiteSpace.test(text)){
-//     newText = text.slice(1) + text.charAt(0).toUpperCase()
-//     console.log(regexTemplateWhiteSpace.test(text))
-//     console.log(newText)
-//     return newText;
-// }
-// return text;
-// }
